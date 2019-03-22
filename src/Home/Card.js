3c2ef7074a4fd66c877d5c7c0f7b1e2a735e6923
Santiago <santylore4@gmail.com>
2019-03-22 01:50:01 -0300

    Adding StyleComponent to Card

--- src/Home/Card.js
+++ src/Home/Card.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import { useSpring, animated } from 'react-spring'
+import styled from 'styled-components'
 
 import './Card.css'
 import Title from './Title'
 
+const CardComponent = styled(animated.div)`
+  width: 35%;
+  height: 45%;
+  border-radius: 15px;
+  transition: box-shadow 0.5s;
+  will-change: transform;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 170, (x - window.innerWidth / 2) / 170, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 export default function Card() {
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   return (
-    <animated.div
-      className='card'
+    <CardComponent
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
       style={{ transform: props.xys.interpolate(trans) }}
     >
       <Title/>
-    </animated.div>
+    </CardComponent>
   )
 }
