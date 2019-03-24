626e53e2a07ee1364dbe0a92d4d1c145a5dae338
Santiago <santylore4@gmail.com>
2019-03-24 16:57:59 -0300

    Responsive header added.

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -4,8 +4,6 @@ import styled from 'styled-components'
 import logo from './logo.png'
 
 const Wrapper = styled.header`
-  justify-content: space-between;
-  flex-direction: row;
   align-items: center;
   display: flex;  
   height: 100px;
@@ -24,20 +22,24 @@ const Logo = styled.div`
   width: 120px;
 `
 const Menu = styled.ul`
-  justify-content: space-between;
+  justify-content: space-evenly;
   list-style-type: none;
   flex-direction: row;
   display: flex;
   width: 900px;
+
+  @media (max-width: 1150px) {
+    display: none
+  }
 `
 const ItemContainer= styled.li``
 
-const Icon = styled.div`
-  width: 30%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+// const Icon = styled.div`
+//   width: 30%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
 
 
 export const Item = styled.a`
@@ -53,54 +55,60 @@ export const Item = styled.a`
   }
 `
 
-const IconsContainer = styled.div`
-  height: 100px;
-  width: 200px;
-  margin-right: 30px;
+const Container = styled.div`
+  height: 100%;
+  width: 1400px;
   display: flex;
   flex-direction: row;
-  align-items: center
+  align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 1650px) {
+    width: 1300px;
+  }
+
+  @media (max-width: 1600px) {
+    width: 1200px;
+  }
+  @media (max-width: 1300px) {
+    width: 1000px;
+  }
+  @media (max-width: 1150px) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 `
 
 export default function Header() {
   return(
     <Wrapper>
-      <Logo>
-        <a href='http://localhost:3000/'>
-          <img src={logo} alt='logo'/>
-        </a>
-      </Logo>
-      <div>
-        <Menu>
-          <ItemContainer>
-            <Item href='http://localhost:3000/'>HOME</Item>
-          </ItemContainer>
-          <ItemContainer>
-            <Item href='#About'>ABOUT US</Item>
-          </ItemContainer>
-          <ItemContainer>
-            <Item href='#OurWork'>WORK</Item>
-          </ItemContainer>
-          <ItemContainer>
-            <Item href='#Location'>LOCATION</Item>
-          </ItemContainer>
-          <ItemContainer>
-            <Item href='#Contact'>CONTACT</Item>
-          </ItemContainer>
-        </Menu>
-      </div>
-      <IconsContainer>
-        <Icon>
-          <Item href='http://localhost:3000/'><i className="fab fa-instagram fa-2x"></i></Item>
-        </Icon>
-        <Icon>
-          <Item href='http://localhost:3000/'><i className="fab fa-github fa-2x"></i></Item>
-        </Icon>
-        <Icon>
-          <Item href='http://localhost:3000/'><i className="fab fa-facebook fa-2x"></i></Item>
-        </Icon>
-      </IconsContainer>
+      <Container>
+        <Logo>
+          <a href='http://localhost:3000/'>
+            <img src={logo} alt='logo'/>
+          </a>
+        </Logo>
+        <div>
+          <Menu>
+            <ItemContainer>
+              <Item href='http://localhost:3000/'>HOME</Item>
+            </ItemContainer>
+            <ItemContainer>
+              <Item href='#About'>ABOUT US</Item>
+            </ItemContainer>
+            <ItemContainer>
+              <Item href='#OurWork'>WORK</Item>
+            </ItemContainer>
+            <ItemContainer>
+              <Item href='#Location'>LOCATION</Item>
+            </ItemContainer>
+            <ItemContainer>
+              <Item href='#Contact'>CONTACT</Item>
+            </ItemContainer>
+          </Menu>
+        </div>
+      </Container>
     </Wrapper>
   )
 }
